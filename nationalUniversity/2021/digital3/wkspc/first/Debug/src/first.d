0163574f290328b2c67d06972a5e1ade1570b4b7
Clifferto <ferrarisdomingo17@gmail.com>
2021-08-26 15:09:21 -0300

    [2021] Agregado digitales3

---
+++ nationalUniversity/2021/digital3/wkspc/first/Debug/src/first.d
@@ -0,0 +1,11 @@
+src/first.o src/first.d: ../src/first.c \
+ /media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/LPC17xx.h \
+ /media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cm3.h \
+ /media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cmInstr.h \
+ /media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cmFunc.h \
+ /media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h
+/media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/LPC17xx.h:
+/media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cm3.h:
+/media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cmInstr.h:
+/media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/core_cmFunc.h:
+/media/cliff/Desk/UNC/3digital/wkspc/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h:
