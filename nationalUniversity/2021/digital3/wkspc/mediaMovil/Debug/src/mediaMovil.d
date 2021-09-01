c22454da128a786fa0930ab599c6e1f179ea630a
Clifferto <ferrarisdomingo17@gmail.com>
2021-09-01 17:46:36 -0300

    [010921] Punto 2 del lab1

---
+++ nationalUniversity/2021/digital3/wkspc/mediaMovil/Debug/src/mediaMovil.d
@@ -0,0 +1,11 @@
+src/mediaMovil.o src/mediaMovil.d: ../src/mediaMovil.c \
+ /media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/LPC17xx.h \
+ /media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cm3.h \
+ /media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cmInstr.h \
+ /media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cmFunc.h \
+ /media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h
+/media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/LPC17xx.h:
+/media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cm3.h:
+/media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cmInstr.h:
+/media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/core_cmFunc.h:
+/media/cliff/Desk/UNC/carrerpath/nationalUniversity/2021/digital3/wkspc/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h:
