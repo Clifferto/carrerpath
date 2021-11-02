82b40e340ef84aea1e43c043553faad70a535e2a
Clifferto <ferrarisdomingo17@gmail.com>
2021-11-02 00:47:14 -0300

    [21121] Mejorada señal de entrada y salida del sistema, agregados filtros y promedio de muestras. Falta integrar con modulos nRF, y pasar configuraciones de perisfericos a librerias .h.

--- nationalUniversity/2021/digital3/test.py
+++ nationalUniversity/2021/digital3/test.py
@@ -6,51 +6,110 @@ import control as ct
 import sympy as sp
 from sympy.core import symbol
 from sympy.core.function import expand
+from sympy.matrices.common import NonInvertibleMatrixError
 from sympy.polys.polyoptions import Symbols
  
-# #impedancias
-# s,R1,R2,R3,C1,C2,C3,Ad0,Wh=sp.symbols("s,R1,R2,R3,C1,C2,C3,Ad0,Wh")
+#impedancias
+s,Vi,Vo,R1,R2,R3,C1,C2,C3,Ad0,Wh=sp.symbols("s,Vi,Vo,R1,R2,R3,C1,C2,C3,Ad0,Wh")
 # z1=1/(s*C1)+R1
 # z2=1/(s*C2+1/R2)
 # Ad=Ad0/(1+s/Wh)
 # z3=R3/(R3+1/(s*C3))
 
-# # z1=sp.sympify("1/(s*C1)+R1")
-# # z2=sp.sympify("1/(s*C2+1/R2)")
+#pasa banda pasivo
+#eq de thevenin en la entrada
+Vth=Vi*(1/(s*C1))/(1/(s*C1)+R1)
+Zth=1/(s*C1+1/R1)
 
-# sp.pprint(sp.factor(-(z2/z1)*Ad*z3))
-##################################################3
-with open("sign","r") as sin:
-    lin=sin.readline()
-    data=np.array(lin.split(",")[0:-1],dtype=("uint16"))
-    print(data)
+#tension de salida
+Vo=Vth*R2/(R2+1/(s*C2)+Zth)
 
-    plt.figure()
-    plt.plot(data)
-    plt.grid()
-    plt.show()
+#respuesta del filtro
+BPF=sp.factor(Vo/Vi)
+
+nn=sp.numer(BPF)
+dd=sp.denom(BPF)
+
+nn=nn*(1/(C1*R1*C2*R2))
+dd=sp.collect(sp.expand(dd*(1/(C1*R1*C2*R2))),s)
+
+BPF=nn/dd
+sp.pprint(BPF)
+
+G1=(1/(s*C1)/(1/(s*C1)+R1)).factor()
+G2=(R2/(R2+1/(s*C2))).factor()
+
+BPFi=(G1*G2).expand()
+nni=sp.numer(BPFi)
+ddi=sp.denom(BPFi)
+
+nni=nni*(1/(C1*R1*C2*R2))
+ddi=sp.collect(sp.expand(ddi*(1/(C1*R1*C2*R2))),s)
+sp.pprint(nni/ddi)
+
+Zo=Zth
+Zi=1/(s*C2)
+
+print("-"*60)
+sp.pprint(sp.Abs(Zo)**2)
+print("<<<")
+sp.pprint(sp.Abs(Zi)**2)
+
+print("-"*60)
+modo=1/((s*C1)**2+(1/R1)**2)
+modi=Zi**2
+sp.pprint(1/modo)
+print(">>>")
+sp.pprint(1/modi)
+
+print("\nSi R1 > 1, Entonces: C1 > 10*C2 --> w1 << w2\n")
+
+
+
+#denominador
+a=dd.coeff(s,2)
+b=dd.coeff(s,1)
+c=dd.coeff(s,0)
+
+wp12=((-b+sp.sqrt(b**2-4*a*c))/(2*a))
+#sp.pprint(wp12)
+
+# z1=sp.sympify("1/(s*C1)+R1")
+# z2=sp.sympify("1/(s*C2+1/R2)")
+
+##################################################
+
+# with open("sign","r") as sin:
+#     lin=sin.readline()
+#     data=np.array(lin.split(",")[0:-1],dtype=("uint16"))
+#     print(data)
+
+#     plt.figure()
+#     plt.plot(data)
+#     plt.grid()
+#     plt.show()
     
-exit()
-#ejes
-naxis=np.arange(-512,512)
-print(len(naxis))
+# exit()
+# #ejes
+# naxis=np.arange(-512,512)
+# print(len(naxis))
 
-# Funciones
-#impulso
-resp=np.array([1 if n==0 else 0 for n in naxis],dtype="uint8")
+# # Funciones
+# #impulso
+# resp=np.array([1 if n==0 else 0 for n in naxis],dtype="uint8")
 
-print(resp.shape)
-plt.figure()
-plt.stem(resp)
-plt.grid()
-plt.show()
+# print(resp.shape)
+# plt.figure()
+# plt.stem(resp)
+# plt.grid()
+# plt.show()
 
-lin=0
-with open("respuestaDiscreta","w") as fil:
-    for i in resp:
-        if lin%55==0: 
-            fil.write(f"\n")
-        else: 
-            fil.write(f"{i},")
+# lin=0
+# with open("respuestaDiscreta","w") as fil:
+#     for i in resp:
+#         if lin%55==0: 
+#             fil.write(f"\n")
+#         else: 
+#             fil.write(f"{i},")
         
-        lin+=1
+#         lin+=1
--- nationalUniversity/2021/digital3/wkspc1/final/src/final.c
+++ nationalUniversity/2021/digital3/wkspc1/final/src/final.c
@@ -1,10 +1,12 @@
 /*
 ===============================================================================
- Name        : ED3_proyecto_final_uart_LPC.c
+ Name        : ED3_proyecto_final_LPC.c
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
- Description :
+ Description : (TX) Graba el mensaje, tiene una clave configurable, envia los datos
+               a través de modulo nrf, interfaz con usuario (Script MATLAB)
+			   mediante UART.
 ===============================================================================
 */
 
@@ -21,6 +23,7 @@
 
 #include <cr_section_macros.h>
 
+//Macros---
 #define MULVAL 14
 #define DIVADDVAL 2
 #define Ux_FIFO_EN (1<<0)
@@ -30,139 +33,166 @@
 #define Rx_RDA_INT (1<<0)
 #define MASK_INT_ID (0xE)
 #define MASK_RDA_INT (2)
-#define LPC_UART_0 (LPC_UART_TypeDef*) LPC_UART0_BASE
-//#define LED_RED_TOGGLE() LPC_GPIO0 -> FIOPIN = (LPC_GPIO0 -> FIOPIN) ^ (1 << 22)
+//seleccion de UART
+#define UARTx (LPC_UART_TypeDef*) LPC_UART2_BASE
 #define LED_RED_TOGGLE() LPC_GPIO0 -> FIOPIN ^= (1 << 22)
 #define LED_BLUE_TOGGLE() LPC_GPIO3 -> FIOPIN ^= (1 << 26)
-//#define LED_GREEN_TOGGLE() LPC_GPIO3 -> FIOPIN = (LPC_GPIO3 -> FIOPIN) ^ (1 << 25)
 #define LED_GREEN_TOGGLE() LPC_GPIO3 -> FIOPIN ^= (1 << 25)
-#define TIME 1000000U
 #define IN_BUFF_LEN 12000u
 
-//xxx
+//Variables y punteros---
 uint32_t currentKey;
 uint8_t readyToLoad, flagAdcDone;
 uint8_t cmd[3];
 uint16_t inBuff[IN_BUFF_LEN];
 uint16_t *pInBuff = inBuff;
 
-void initUART0(void);
+//xxx Prototipos---
+void initUARTx(void);
 void initLEDPins(void);
 void initADC(void);
-void initDAC(void);
 void initTMR0(void);
 void initTMR2(void);
 void blinkRedLed(void);
 void blinkGreenLed(void);
 void blinkBlueLed(void);
 void delay_ms(int);
+//opcionales
+void initDAC(void);
 void initRit();
-void dacPlay(void);
 
-//xxx
+/*
+ * xxx Main loop
+ */
 int main(void) {
+	/* System Clock Init */
+	SystemInit();
+
 	/* Init */
 	initLEDPins();
-	initUART0();
+	initUARTx();
 	initADC();
 	initTMR0();
 	initTMR2();
-
+	//opcionales
 	initRit();
 	initDAC();
 
 	/* Algorithm */
-    currentKey = 0x12345678; //valor por defecto
+	//Default key
+    currentKey = 0x12345678; 
     readyToLoad = 1;
+
 	while(1){
-		//escuchar comandos
-		UART_Receive(LPC_UART_0, (uint8_t *)cmd, sizeof(cmd), BLOCKING);
+		//Listen dot commands
+		UART_Receive(UARTx, (uint8_t *)cmd, sizeof(cmd), BLOCKING);
 
+		/*
+		* Decide que comando ejecutar:
+		* '.g': Get current Key
+		* '.s': Set a new Key
+		* '.r': Record and send message
+		* '.t': Transmit message by SPI (falta)
+		*/
+		//if dot command
 		if (cmd[0] == '.') {
-			/*
-			 * decide que comando ejecutar:
-			 * 'g': Get current Key
-			 * 's': Set a new Key
-			 * 'r': Record and send message (FALTA)
-			 */
+			//recognize command
 			switch (cmd[1]) {
-			case 'g':
-				//Get current key
-				UART_Send(LPC_UART_0, (uint8_t *)&currentKey, sizeof(currentKey), BLOCKING);
-				blinkRedLed();
-				break;
-			case 's':
-				//Set a new key
-				UART_Send(LPC_UART_0, &readyToLoad, sizeof(readyToLoad), BLOCKING);
-				UART_Receive(LPC_UART_0, (uint8_t *)&currentKey, sizeof(currentKey), BLOCKING);
-				blinkGreenLed();
-				break;
-			case 'r':
-				flagAdcDone=0;
-				//Record a 2sec message
-				TIM_ResetCounter(LPC_TIM0);
-				TIM_Cmd(LPC_TIM0, ENABLE);
-				while(!flagAdcDone) {};
-				TIM_Cmd(LPC_TIM0, DISABLE);
-				blinkBlueLed();
+				//getkey
+				case 'g':
+					//Get current key, and blink red led
+					UART_Send(UARTx, (uint8_t *)&currentKey, sizeof(currentKey), BLOCKING);
+					blinkRedLed();
+					break;
+				//setkey
+				case 's':
+					//Set a new key, and blink green led
+					UART_Send(UARTx, &readyToLoad, sizeof(readyToLoad), BLOCKING);
+					UART_Receive(UARTx, (uint8_t *)&currentKey, sizeof(currentKey), BLOCKING);
+					blinkGreenLed();
+					break;
+				//record
+				case 'r':
+					flagAdcDone=0;
+					//Record a 2sec message, and blink blue led
+					TIM_ResetCounter(LPC_TIM0);
+					TIM_Cmd(LPC_TIM0, ENABLE);
+					while(!flagAdcDone) {};
 
-				//reproducir señal por dac, activar rit
-				RIT_Cmd(LPC_RIT, ENABLE);
+					TIM_Cmd(LPC_TIM0, DISABLE);
+					blinkBlueLed();
+
+					//rit on, start playing signal through DAC
+					//RIT_Cmd(LPC_RIT, ENABLE);
+					break;
 			}
+			//clean command buffer
 			cmd[0]=0;
 			cmd[1]=0;
+			cmd[2]=0;
 		}
 	};
     return 0 ;
 }
 
-void initUART0(void)
+/*
+ * Config UART
+ * * BaudRate= ~115200(114882) (si PCLK=25MHz, lo es por defecto)
+ * * P0.2 TX0
+ * * P0.3 RX0
+ */
+void initUARTx(void)
 {
+	//p0.10 y p0.11 to UART2, pull-up
+	PINSEL_CFG_Type cfgTXRX={
+		.Funcnum=1,
+		.Pinnum=10,
+		.Portnum=0
+	};
+	PINSEL_ConfigPin(&cfgTXRX);
+
+	cfgTXRX.Pinnum=11;
+	PINSEL_ConfigPin(&cfgTXRX);
+
+	//UART 115200 bauds, 8 bits data, 1 stop bit, no parity
+	LPC_SC->PCONP |= 1<<24; //enciendo UART2 (por default encendido)
+
+	LPC_UART2->LCR = 3|DLAB_BIT ; //8 bits, sin paridad, 1 Stop bit y DLAB enable
+	//set baud rate
+	LPC_UART2->DLL = 12;
+	LPC_UART2->DLM = 0;
+	LPC_UART2->FDR = (MULVAL<<4)|DIVADDVAL; //MULVAL=15(bits - 7:4), DIVADDVAL=2(bits - 3:0)
+
 	/*
-	 * BaudRate= ~115200(114882) (si PCLK=25MHz, lo es por defecto)
-	 * P0.2 TX0
-	 * P0.3 RX0
+	 * Block divisor latches
 	 */
+	LPC_UART2->LCR &= ~(DLAB_BIT); //desactivando DLAB lockeamos los divisores para el Baudrate
 
-//	/* Init con drivers */
-//	UART_CFG_Type uart0Config;
-//	uart0Config.Baud_rate = 115200;
-//	uart0Config.Databits = UART_DATABIT_8;
-//	uart0Config.Parity = UART_PARITY_NONE;
-//	uart0Config.Stopbits = UART_STOPBIT_1;
-//	UART_Init(LPC_UART_0, &uart0Config);
-//	UART_IntConfig(LPC_UART_0, UART_INTCFG_RBR, ENABLE);
+	LPC_UART2->FCR |= Ux_FIFO_EN|Rx_FIFO_RST|Tx_FIFO_RST;
 
-	LPC_PINCON->PINSEL0 |= (1<<4)|(1<<6); //funcion TXD0/RXD0 para los pines P0.2/P0.3
-	//LPC_SC->PCONP |= 1<<3; //enciendo UART0 (por default encendido)
-
-	LPC_UART0->LCR = 3|DLAB_BIT ; //8 bits, sin paridad, 1 Stop bit y DLAB enable
-	LPC_UART0->DLL = 12;
-	LPC_UART0->DLM = 0;
-	LPC_UART0->FDR = (MULVAL<<4)|DIVADDVAL; //MULVAL=15(bits - 7:4), DIVADDVAL=2(bits - 3:0)
-	//bloquear divisor latches
-	LPC_UART0->LCR &= ~(DLAB_BIT); //desactivando DLAB lockeamos los divisores para el Baudrate
-
-	//LPC_UART0->IER |= Rx_RDA_INT; //int cada vez que se reciban cierto numero de char (1 en este caso)
-	LPC_UART0->FCR |= Ux_FIFO_EN|Rx_FIFO_RST|Tx_FIFO_RST;
-
-	//NVIC_ClearPendingIRQ(UART0_IRQn);
-	//NVIC_EnableIRQ(UART0_IRQn);
+	//	//UART 115200 bauds, 8 bits data, 1 stop bit, no parity
+	//	UART_CFG_Type cfgUart;
+	//	cfgUart.Baud_rate = 115200;
+	//	cfgUart.Databits = UART_DATABIT_8;
+	//	cfgUart.Parity = UART_PARITY_NONE;
+	//	cfgUart.Stopbits = UART_STOPBIT_1;
+	//	UART_Init(UARTx, &cfgUart);
 }
 
+/*
+ * Config LEDs
+ * * P0.22 Red
+ * * P3.26 Blue
+ * * P3.25 Green
+ */
 void initLEDPins(void) {
-	/*
-	 * P0.22 Red
-	 * P3.26 Blue
-	 * P3.25 Green
-	 */
+
 	const uint8_t ledNum[] = {22, 26, 25};
 	const uint8_t ledPort[] = {0, 3, 3};
 
 	PINSEL_CFG_Type ledPin;
 	ledPin.Pinmode = PINSEL_PINMODE_PULLUP;
 	ledPin.Funcnum = 0;
-
 
 	for (int i = 0;i < 3; i++) {
 		ledPin.Pinnum = ledNum[i];
@@ -171,16 +201,19 @@ void initLEDPins(void) {
 		GPIO_SetDir(ledPort[i], 1<<ledNum[i], 1);
 	}
 
-	//dejar pines en alto (LEDS OFF)
+	//set higth level (LEDS OFF)
 	GPIO_SetValue(0, 1<<22);
 	GPIO_SetValue(3, 1<<25);
 	GPIO_SetValue(3, 1<<26);
 }
 
+/*
+ * Config ADC
+ * * P0.23 to AD0.0
+ * * pull-off
+ */
 void initADC(void) {
-	/*
-	 * P0.23 AD0[0]
-	 */
+
 	PINSEL_CFG_Type ADC_pin;
 	ADC_pin.Pinnum = 23;
 	ADC_pin.Portnum = 0;
@@ -193,29 +226,62 @@ void initADC(void) {
 	LPC_SC-> PCLKSEL0 |= (3 << 24);  // CCLK/8 = 100Mhz/8 = 12.5 MHz
 	ADC_Init(LPC_ADC, 192307); //enciende PCONP, da PDN = 1 y carga un CLKDIV medio choto
 	ADC_ChannelCmd(LPC_ADC, 0, ENABLE); //selecciona bits SEL, o sea el canal habiliado, recordar que es modo hardware
-	ADC_BurstCmd(LPC_ADC, DISABLE); //configura BURST de ADCR
+	//ADC_BurstCmd(LPC_ADC, DISABLE); //configura BURST de ADCR
 	ADC_StartCmd(LPC_ADC, ADC_START_ON_MAT01); //configura START de ADCR
 	ADC_EdgeStartConfig(LPC_ADC, ADC_START_ON_FALLING); //configura EDGE de ADCR
 	ADC_IntConfig(LPC_ADC, ADC_ADGINTEN, SET); //configura individualmente las int por canales o la global
 
 	NVIC_ClearPendingIRQ(ADC_IRQn);
 	NVIC_EnableIRQ(ADC_IRQn);
+
+	//AD0.1-AD0.7 a GPIO, pull-down, salida digital, estado bajo
+	//tener en cuenta: AD0.3 DACOUT, AD0.6 y AD0.7 UART0
+	unsigned char nullCh[6]={2,3,24,25,30,31};
+
+	PINSEL_CFG_Type cfgNullCh={
+		.Funcnum=0,
+		.OpenDrain=PINSEL_PINMODE_NORMAL,
+		.Pinmode=PINSEL_PINMODE_PULLDOWN,
+	};
+
+	for(unsigned char ch=0;ch<sizeof(nullCh);ch++){
+		//ch del puerto 0
+		if(ch<4){
+			//pull-down, pin como salida
+			cfgNullCh.Portnum=0;
+			cfgNullCh.Pinnum=*(nullCh+ch);
+			PINSEL_ConfigPin(&cfgNullCh);
+			GPIO_SetDir(0, 1<<(*(nullCh+ch)), 1);
+			//estado bajo
+			GPIO_ClearValue(0, 1<<(*(nullCh+ch)));
+		}
+
+		//ch del puerto 1
+		else{
+			//pull-down, pin como salida
+			cfgNullCh.Portnum=1;
+			cfgNullCh.Pinnum=*(nullCh+ch);
+			PINSEL_ConfigPin(&cfgNullCh);
+			GPIO_SetDir(1, 1<<(*(nullCh+ch)), 1);
+			//estado bajo
+			GPIO_ClearValue(1, 1<<(*(nullCh+ch)));
+		}
+	}
 }
 
+/*
+ * Config TMR0
+ * * Recordar F_MRx = 2*prom*F_s(deseada)
+ * * F_s = 3 KHz, prom = 8 -> F_MRx = 48 KHz -> 20.8us
+ * * Con 12us -> F_s(real)= 47.619 KHz
+ */
 void initTMR0(void) {
-	/*
-	 * No hace falta configurar PINSEL
-	 * Recordar F_MRx = 2*F_s(deseada)
-	 * F_s = 6 KHz -> F_MRx = 12 KHz
-	 * T_MRx = 83.33 us = res*(MRx+1)
-	 * MRx = (T_MRx/res)-1 = 82.33 ~= 83
-	 * F_s'= 5.952 KHz
-	 */
+
 	LPC_SC -> PCONP |= (1 << 1); //enciende TMR0 (recordar que ya lo esta por defecto);
 	//LPC_SC -> PCLKSEL0 |= (1 << 2); //CCLK/1
 	LPC_TIM0 -> EMR |= (3 << 6); //toglear (funcion 3) EM0 (MAT0.1)
-	LPC_TIM0 -> PR = 24; //si PCLK = 25MHz entonces con ese PR se logra resolución de 1us
-	LPC_TIM0 -> MR1 = 83;
+	LPC_TIM0 -> PR = 25-1; //si PCLK = 25MHz entonces con ese PR se logra resolución de 1us
+	LPC_TIM0 -> MR1 = 21-1;
 	LPC_TIM0 -> MCR |= (2 << 3); //solo resetar cuando se llegue match, no int, no stop
 	//LPC_TIM0 -> TCR = 3; //enable y reset en 1
 	//LPC_TIM0 -> TCR &= ~(2); //quito el reset
@@ -234,30 +300,50 @@ void initTMR2(void) {
 	TIM_Init(LPC_TIM2, TIM_TIMER_MODE, &timer2_conf); //TIM_Init no le da enable
 }
 
-//xxx
-void UART0_IRQHandler(void) {
-	uint32_t uartIntSt = UART_GetIntId(LPC_UART_0); //valor del registro IIR
-
-	if (((uartIntSt & MASK_INT_ID) >> 1) == MASK_RDA_INT) {
-		UART_Receive(LPC_UART_0, &cmd[1], sizeof(cmd), BLOCKING);
-	}
-	//en teoria se limpia el flag cuando se efectua la lectura de los datos
-}
+//suma para promediar
+unsigned int acc0=0;
+unsigned char samp=0;
+unsigned char n=8;
 
 //xxx
 void ADC_IRQHandler(void) {
 	volatile uint16_t ADC_value;
 	ADC_value = (ADC_GlobalGetData(LPC_ADC) >> 4) & 0xFFF;
-	*pInBuff = ADC_value;
 
-	if(pInBuff == &inBuff[IN_BUFF_LEN-1]) {
-		pInBuff = inBuff;
-		flagAdcDone = 1;
+	//aumentar numero de muestra
+	samp++;
+
+	//acumular
+	acc0+=ADC_value;
+
+	/*
+	 * todo Correjir velocidad de reproduccion
+	 */
+	//si se cumplen las n muestras
+	if(samp>=n){
+		//promediar y actualizar dac
+		acc0>>=3;
+		DAC_UpdateValue(LPC_DAC, (acc0>>2)&0x3FF);
+
+		//reiniciar variables
+		acc0=0;
+		samp=0;
 	}
-	else {
-		pInBuff++;
-		flagAdcDone = 0;
-	}
+
+	/*
+	 * todo Reemplazar por logica del buffer
+	 */
+
+//	*pInBuff = ADC_value;
+//
+//	if(pInBuff == &inBuff[IN_BUFF_LEN-1]) {
+//		pInBuff = inBuff;
+//		flagAdcDone = 1;
+//	}
+//	else {
+//		pInBuff++;
+//		flagAdcDone = 0;
+//	}
 }
 
 void blinkRedLed(void) {
@@ -283,6 +369,7 @@ void blinkBlueLed(void) {
 	//LED_BLUE_TOGGLE();
 	LPC_GPIO3->FIOPIN^=(1<<26);
 }
+
 void delay_ms(int time) {
 	TIM_ResetCounter(LPC_TIM2);
 	TIM_Cmd(LPC_TIM2, ENABLE);
@@ -331,7 +418,7 @@ void RIT_IRQHandler(){
 	RIT_GetIntStatus(LPC_RIT);
 
 	//actualizar valor del dac
-	DAC_UpdateValue(LPC_DAC, (*pInBuff)&0x3FF);
+	DAC_UpdateValue(LPC_DAC, ((*pInBuff)>>4)&0x3FF);
 
 	//incrementar puntero del buffer
 	if(pInBuff==&inBuff[IN_BUFF_LEN-1]){
@@ -342,9 +429,5 @@ void RIT_IRQHandler(){
 	}
 	else pInBuff++;
 
-	return;
-}
-
-void dacPlay(void){
 	return;
 }
