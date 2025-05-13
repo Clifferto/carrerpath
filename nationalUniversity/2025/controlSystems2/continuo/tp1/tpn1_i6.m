304412650b113b745ab946a6aca470cbe1913d60
Clifferto <ferrarisdomingo17@gmail.com>
2025-05-13 15:48:12 -0300

    updated item6, updated informe

--- nationalUniversity/2025/controlSystems2/continuo/tp1/tpn1_i6.m
+++ nationalUniversity/2025/controlSystems2/continuo/tp1/tpn1_i6.m
@@ -9,17 +9,13 @@ pkg load io
 addpath('../../lib');
 mylib
 
-function [Y, X, U, err] = sys_model(A, B, C, D, in, t, x0)
+function [Y, X, U, err] = sys_model(A, B, C, D, in, t, x0, pid_config)
     % paso igual a la resolucion temporal
-    h   = t(2) - t(1)
+    h   = t(2) - t(1);
 
-    % parametros del PID (Tip: partir de KP=0,1;Ki=0,01; KD=5)
-    Kp  = 13;
-    Ki  = 100;
-    Kd  = 1;
-    % Kp  = 200;
-    % Ki  = 2000;
-    % Kd  = 5;
+    Kp  = pid_config(1);
+    Ki  = pid_config(2);
+    Kd  = pid_config(3);
 
     Ts  = h;
     A1  = (2*Kp*Ts + Ki*Ts^2 + 2*Kd)/(2*Ts);
@@ -112,55 +108,54 @@ matD    = [  0   0];
 comment('Simulacion')
 % ! de las graficas de mediciones
 va_amp  = 2;
-% tl_amp  = 0;
 tl_amp  = .12;
 va_t0   = 100E-3;
-% tl_t0   = 5;
 tl_t0   = 700E-3;
 tl_t1   = 800E-3;
 
 [t_step, t_max] = get_time_params(pole(ss(matA, matB, matC, matD)));
 t_step  /= 3
 % t_step  = 1E-3;
-t_max   *= 5
+t_max   *= 7
 % t_max   = 10
 t       = 0:t_step:t_max;
 va      = va_amp*heaviside(t - va_t0);
 tl      = tl_amp*(heaviside(t - tl_t0) - heaviside(t - tl_t1));
 in      = [va ; tl]';
 
+% parametros del PID (Tip: partir de KP=0,1;Ki=0,01; KD=5)
+Kp  = 17;
+Ki  = 100;
+Kd  = .8;
+% Kp  = 200;
+% Ki  = 2000;
+% Kd  = 5;
+pid_config          = [Kp Ki Kd]
 x0                  = [0 ; 0 ; 0];
-[y_est, x, u, err]  = sys_model(matA, matB, matC, matD, in, t, x0);
+[y_est, x, u, err]  = sys_model(matA, matB, matC, matD, in, t, x0, pid_config);
 
 figure;
-subplot(5,1,1);
-plot(t, x(:,1), 'r', 'LineWidth', 2);
-title('State var x_1: ia(t)'); ylabel('ia(t) [A]'); grid;
-legend('ia(t)');
-subplot(5,1,2);
+subplot(3,1,1);
 plot(t, x(:,2), 'r', 'LineWidth', 2);
-title('State var x_2: theta(t)'); ylabel('theta(t) [rad]'); grid;
+title('Output y_1: theta(t)'); ylabel('theta(t) [rad]'); grid;
 legend('theta(t)');
-subplot(5,1,3);
-plot(t, x(:,3), 'r', 'LineWidth', 2);
-title('State var x_3: omega(t)'); ylabel('omega(t) [rad/s]'); grid;
-legend('omega(t)');
-subplot(5,1,4);
+subplot(3,1,2);
 plot(t, in(:,1), 'LineWidth', 2);
 title('Input i_1: va(t)'); ylabel('va(t) [V]'); grid;
 legend('va(t)');
-subplot(5,1,5); 
+subplot(3,1,3); 
 plot(t, in(:,2), 'LineWidth', 2);
 title('Input i_2: tl(t)'); ylabel('tl(t) [Nm]'); grid;
 legend('tl(t)');
 xlabel('Time [s]'); 
 
-close all;
+% close all;
 
 figure;
 subplot(3,1,1);
 plot(t(1:end-1), err, 'r', 'LineWidth', 2);
-title('Error: e(t)'); ylabel('e(t) [rad]'); grid;
+str = sprintf('Error: e(t) vs PID: kp = %0d, ki = %0d, kd = %0d', Kp, Ki, Kd);
+title(str); ylabel('e(t) [rad]'); grid;
 legend('e(t)');
 subplot(3,1,2);
 plot(t, u(:,1), 'r', 'LineWidth', 2);
